304489b42d5e0ed4bc3d6da3da5e692493cf60be
Rostisalv <rostislav444@gmail.com>
2020-09-01 17:29:48 +0300

    Local settings file added

--- project/settings.py
+++ project/settings.py
@@ -80,19 +80,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'yulmarika',
-        'USER' : 'rostislav',
-        'PASSWORD' : 'streetride24',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
-    }
-}
+from .local_settings import *
 
 
 # Password validation
@@ -146,16 +134,7 @@ IMAGE_SIZES =  {
 
 DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DBBACKUP_STORAGE_OPTIONS = {'location':  os.path.join(BASE_DIR, 'media/backup/')}
-DBBACKUP_CONNECTORS = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'yulmarika',
-        'USER' : 'rostislav',
-        'PASSWORD' : 'streetride24',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
-    }
-}
+DBBACKUP_CONNECTORS = DATABASES
 
 # from django.core.management import call_command
 # print(call_command('dbbackup', interactive=True))
