d904d3b1706bf4753cf8edf30e19fe8549cd8e4d
Rostisalv <rostislav444@gmail.com>
2020-09-02 22:13:57 +0300

    Fixed bugs: replacement + for viber

--- apps/filecodes/context_processors.py
+++ apps/filecodes/context_processors.py
@@ -4,5 +4,6 @@ def fron_files(request):
     context = {
         'front_files' :  FileCodes.objects.last()
     }
+    context['front_files'].phone = context['front_files'].phone.replace('+','')
     context['favicon_ext'] = context['front_files'].favicon.name.split('.')[-1]
     return context
