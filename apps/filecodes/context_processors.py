1ed015a3a958733b8850471cd33ccf71352593e1
Rostisalv <rostislav444@gmail.com>
2020-09-02 22:18:18 +0300

    Fixed bugs: replacement non numbers for viber (regex)

--- apps/filecodes/context_processors.py
+++ apps/filecodes/context_processors.py
@@ -1,9 +1,10 @@
 from .models import FileCodes
+import re
 
 def fron_files(request):
     context = {
         'front_files' :  FileCodes.objects.last()
     }
-    context['front_files'].phone = context['front_files'].phone.replace('+','')
+    context['front_files'].phone = re.sub('[^0-9]', '', context['front_files'].phone) 
     context['favicon_ext'] = context['front_files'].favicon.name.split('.')[-1]
     return context
