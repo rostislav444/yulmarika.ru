25b59e41fb5d588fa24c499629615cfc81cb57c4
Rostisalv <rostislav444@gmail.com>
2020-09-14 20:31:32 +0300

    Update

--- project/settings.py
+++ project/settings.py
@@ -157,30 +157,3 @@ CKEDITOR_CONFIGS = {
 USE_TZ = True
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
-        }
-    },
-    'handlers': {
-        'gunicorn': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'verbose',
-            'filename': '/opt/djangoprojects/reports/bin/gunicorn.errors',
-            'maxBytes': 1024 * 1024 * 100,  # 100 mb
-        }
-    },
-    'loggers': {
-        'gunicorn.errors': {
-            'level': 'DEBUG',
-            'handlers': ['gunicorn'],
-            'propagate': True,
-        },
-    }
-}
