99369a89d21b1b7475289a651a91c0fe741a6a7a
Rostisalv <rostislav444@gmail.com>
2020-09-14 21:52:11 +0300

    Update

--- apps/filecodes/context_processors.py
+++ apps/filecodes/context_processors.py
@@ -7,8 +7,9 @@ def fron_files(request):
     order_pk = request.session.get('order')
     if order_pk:
         order = Order.objects.filter(pk=int(order_pk), status__in=['payed','prepered','at_delivry','delivring','delivred','declined'])
-        del request.session['order']
-        Cart().clear()
+        try: del request.session['order']
+        except: pass
+        Cart(request).clear()
 
     try:    coupon = Coupon.objects.get(pk=int(request.session['coupon']))
     except: coupon = None
